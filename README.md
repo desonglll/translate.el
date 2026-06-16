4c3e366acbd6ec8cce37f177b45f4fc871233564
desonglll <lindesong666@163.com>
2026-06-16 15:27:59 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Translate.el
+
+Translate the content in selection area using trans.
+
+## Usage
+
+select an area then:
+
+``` shell
+M-x translate
+```
---
+++ translate.el
@@ -0,0 +1,54 @@
+;;; translate.el --- Translate using trans -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2026 Carl
+;;
+;; Author: Carl <carl@Carls-MacBook-Air.local>
+;; Maintainer: Carl <carl@Carls-MacBook-Air.local>
+;; Created: June 16, 2026
+;; Modified: June 16, 2026
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex text tools unix vc wp
+;; Homepage: https://github.com/desonglll/translate
+;; Package-Requires: ((emacs "24.4"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(defun translate-hello-world()
+  "Hello world from translate-hello-world."
+  (interactive)
+  (message "hello world from translate."))
+
+(defun translate-get-selection()
+  "Get content in selection area."
+  (interactive)
+  (let ((input
+         (if (use-region-p)
+             (buffer-substring-no-properties (region-beginning) (region-end))
+           (thing-at-point 'word t))))
+    (if (string-blank-p input)
+        (progn
+          nil)
+      (progn
+        (string-trim input)))))
+
+(defun translate-get-translation(text)
+  "Get translation of TEXT using trans."
+  (let ((cmd (format "trans -brief :zh %s" (shell-quote-argument text))))
+    (string-trim (shell-command-to-string cmd))))
+
+(defun translate()
+  "Translate."
+  (interactive)
+  (let ((text (translate-get-selection)))
+    (when text
+      (let ((result (translate-get-translation text)))
+        (message "result: %s" result)))))
+
+(provide 'translate)
+;;; translate.el ends here
